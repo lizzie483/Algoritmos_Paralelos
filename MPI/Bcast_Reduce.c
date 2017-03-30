d71dca10de4e910577a230c70d482f4ae0807ba7
Lizeth Pilar <lizethp1128@gmail.com>
2017-03-30 13:46:09 -0500

    Update Bcast_Reduce.c

--- MPI/Bcast_Reduce.c
+++ MPI/Bcast_Reduce.c
@@ -2,15 +2,21 @@ int main(int argc, char *argv[]){
   //Inicializacion de variables
 int me,numprocs;
 float data,res;
+int valor=10;
+float numero=1.2;
+ //Todo lo que se escribe antes del MPI_Init no funciona de forma paralela, sucede una unica vez en el proceso "0" 
 MPI_Init(&argc,&argv);
 MPI_Comm_rank(MPI_COMM_WORLD, &me);
 MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 //MPI_Barrier
-  data=20.5;
-MPI_Bcast(&data,1,MPI_INT,0,MPI_COMM_WORLD);
-  MPI_Barrier(MPI_COMM_WORLD);
-  //Colocar la barrera antes del calculo para que garantize un sincronizmo antes de que la sume se realize
-  //Si hay un proceso que no tiene la data correcta se garantiza de que antes que se reduzca el reduce todo se haya logrado.
+  data=20.5; //este dato esta en todos los procesos
+MPI_Bcast(&data,1,MPI_INT,0,MPI_COMM_WORLD); //Copia en un sitio donde ya esta copiado
+  //Revisar el funcionamiento del codigo comentando el Bcast
+MPI_Barrier(MPI_COMM_WORLD);
+  //Colocar la barrera antes del calculo para que 
+  //garantize un sincronizmo antes de que la sume se realize
+  //Si hay un proceso que no tiene la data correcta se garantiza de 
+  //que antes que se reduzca el reduce todo se haya logrado.
 MPI_Reduce(&data,&res, 1, MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
 
 if(me==0){
